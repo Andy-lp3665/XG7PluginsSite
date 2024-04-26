0986920227c78a377d059a07f30374409172ddad
GT_Andy-lp666 <167588414+Andy-lp3665@users.noreply.github.com>
2024-04-26 20:32:22 +0000

    f12

--- src/app/cadastrar/page.tsx
+++ src/app/cadastrar/page.tsx
@@ -1,32 +1,33 @@
-"use client"
-import "./css/body.scss"
+"use client";
+import "./css/body.scss";
 
 import Logo from "../imgs/logo.png";
 
-
 export default function Site() {
-    return (
-      <>
-  
-        <form action="/login" method='post'>
-          <img src={ Logo.src } alt="Logo" />
+  return (
+    <>
+      <form action="Cadastrar" method="post">
+        <img src={Logo.src} alt="Logo" />
+        <label>
+          {" "}
+          <h2>Faça o seu cadastro</h2>
+        </label>
+        <input
+          type="text"
+          name="nome"
+          placeholder="nome de usuário*"
+          id=""
+          required
+        />
 
-            <input type="text" name="nome" placeholder='nome de usuário*'/>
+        <input type="text" name="login" placeholder="email*" id="" required />
 
-            <input type="text" name="login" placeholder='email*'/>
+        <input type="text" placeholder="senha*" />
 
-            <label htmlFor="data">Data de nascimento opicional</label>
-            <input type="date" name="data" placeholder='data' />
-            
-            <input type="text" placeholder='senha*'/>
+        <input type="password" placeholder="confirmar senha*" id="" required />
 
-            <input type="password" placeholder='confirmar senha*'/>
-            
-            <input type="submit" />
-
-        </form>
-
-      </>
-      
-    );
-  }
+        <input type="submit" id="enviar" />
+      </form>
+    </>
+  );
+}
