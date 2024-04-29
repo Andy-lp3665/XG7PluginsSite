8bf921d25dbf2bdb50bc77409b7c0f2477e2a296
GT_Andy-lp666 <167588414+Andy-lp3665@users.noreply.github.com>
2024-04-29 20:07:17 +0000

    o site ta pica

--- src/app/cadastrar/page.tsx
+++ src/app/cadastrar/page.tsx
@@ -6,28 +6,33 @@ import Logo from "../imgs/logo.png";
 export default function Site() {
   return (
     <>
-      <form action="Cadastrar" method="post">
-        <img src={Logo.src} alt="Logo" />
-        <label>
-          {" "}
-          <h2>Faça o seu cadastro</h2>
-        </label>
-        <input
-          type="text"
-          name="nome"
-          placeholder="nome de usuário*"
-          id=""
-          required
-        />
+      <div className="container">
+        <form action="Cadastrar" method="post">
+          <img src={Logo.src} alt="Logo" />
+          <label>
+            {" "}
+            <h2>Faça o seu cadastro</h2>
+          </label>
+          <input
+            type="text"
+            name="nome"
+            placeholder="Nome de usuário"
+            id=""
+            required
+          />
 
-        <input type="text" name="login" placeholder="email*" id="" required />
+          <input type="text" name="login" placeholder="Email" id="" required />
 
-        <input type="text" placeholder="senha*" />
+          <input type="text" placeholder="Senha" />
 
-        <input type="password" placeholder="confirmar senha*" id="" required />
+          <input type="password" placeholder="Confirmar senha" id="" required />
 
-        <input type="submit" id="enviar" />
-      </form>
+          <input type="submit" id="enviar" value={"Entrar"} />
+        </form>
+        <a href="./">
+          <button id="b1"> Cancelar</button>
+        </a>
+      </div>
     </>
   );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,28 @@
-"use client"
-import { useState } from 'react';
-import "./css/body.scss"
+"use client";
+import { useState } from "react";
+import "./css/body.scss";
 
 import Logo from "../imgs/logo.png";
 
-
 export default function Site() {
-    return (
-      <>
-  
-        <form action="/login" method='post'>
-          <img src={ Logo.src } alt="Logo" />
-            <label htmlFor="login">login</label>
-            <input type="text" name="login" placeholder='email'/>
+  return (
+    <>
+      <div className="container">
+        <form action="/login" method="post">
+          <img src={Logo.src} alt="Logo" />
+          <label htmlFor="login">
+            <h1>Entre</h1>{" "}
+          </label>
+          <input type="text" name="login" placeholder="Email" required />
 
-            <input type="text" placeholder='senha'/>
-            
-            <input type="submit" />
-            <a href="../cadastrar">Não tem uma conta?</a>
+          <input type="text" placeholder="senha" required />
 
+          <input type="submit" id="entrada" value={"Entrar"} />
         </form>
-
-      </>
-      
-    );
-  }
+        <a href="cadastrar">
+          <button id="cancelar1">Não tem uma conta?</button>
+        </a>
+      </div>
+    </>
+  );
+}
